68057f96a18524a88707cdde1391b1644a329e39
Zagrivaya <zagrivaya@yandex.ru>
2023-06-23 14:16:34 +0300

    init commit

---
+++ src/test/java/ru/netology/geo/GeoServiceImplTest.java
@@ -0,0 +1,29 @@
+package ru.netology.geo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
+
+class GeoServiceImplTest {
+
+    @Test
+    public void testByIpRussia() {
+        GeoServiceImpl geoService = new GeoServiceImpl();
+
+        Country actual = geoService.byIp("172.").getCountry();
+        Country expected = new Location(null, Country.RUSSIA, null, 0).getCountry();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testByIpUSA() {
+        GeoServiceImpl geoService = new GeoServiceImpl();
+
+        Country actual = geoService.byIp("96.").getCountry();
+        Country expected = new Location(null, Country.USA, null, 0).getCountry();
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
---
+++ src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
@@ -0,0 +1,29 @@
+package ru.netology.i18n;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.entity.Country;
+
+class LocalizationServiceImplTest {
+
+    @Test
+    public void testLocaleRussia() {
+        LocalizationService localizationService = new LocalizationServiceImpl();
+
+        String actual = localizationService.locale(Country.RUSSIA);
+        String expected = "Добро пожаловать";
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void testLocaleOther() {
+        LocalizationService localizationService = new LocalizationServiceImpl();
+
+        String actual = localizationService.locale(Country.BRAZIL);
+        String expected = "Welcome";
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+}
